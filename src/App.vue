31b4ce3286496b56e0649d4c2428d48a49a9bb84
macro <haoqiuran126@126.com>
2017-03-18 18:00:19 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="app">
+  <h1>{{title}}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  data: function () {
+    return {
+      title: 'This is a todo list demo'
+    };
+  }
+};
+</script>
+
+<style>
+html{
+  height: 100%;
+}
+body{
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  height: 100%;
+}
+</style>
